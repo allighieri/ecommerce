9fb066bc621af22468cfc82c94e7e31d15083271
allighieri <agenciaolhardigital@gmail.com>
2020-06-15 16:44:06 -0300

    Tela de login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,18 +9,22 @@ class Page{
     private $tpl;
     private $options = [];
     private $defaults = [
+        "header"=>true,
+        "footer"=>true,
         "data"=>[]
     ];
 
     public function __construct($opts = array(), $tpl_dir = "/views/"){
 
-       $this->options = array_merge($this->defaults, $opts);
+        //$this->defaults["data"]["session"] = $_SESSION;
+       
+        $this->options = array_merge($this->defaults, $opts);
        
         $config = array(
             "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
             "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
             "debug"         => false // set to false to improve the speed
-           );
+        );
 
         Tpl::configure( $config );
 
@@ -28,7 +32,7 @@ class Page{
         
         $this->setData($this->options["data"]);
 
-        $this->tpl->draw("header");
+        if ($this->options["header"] === true) $this->tpl->draw("header");
 
     }
 
@@ -51,7 +55,7 @@ class Page{
 
     public function __destruct(){
         
-        $this->tpl->draw("footer");
+        if ($this->options["footer"] === true) $this->tpl->draw("footer");
 
     }
 
