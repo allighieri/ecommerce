917f356a092d7fe112ea14c0768e483fd6f87f68
allighieri <agenciaolhardigital@gmail.com>
2020-06-15 22:42:20 -0300

    Crud user

--- views-cache/login.b18aae8b7bfe6bd31c1fe7cf29d9e8b3.rtpl.php
+++ views-cache/login.b18aae8b7bfe6bd31c1fe7cf29d9e8b3.rtpl.php
@@ -27,7 +27,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="../../index2.html"><b>Hcore</b>Curso</a>
+    <a href="../../"><b>Hcore</b>Curso</a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
